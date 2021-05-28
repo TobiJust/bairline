21281445e72ecec1d4c45018a20bbebd431cb332
Just, Tobias T. (415) <tobias.t.just@daimler.com>
2021-05-28 14:10:10 +0200

    Uses static target

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  target: 'static', // default is 'server'
+  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxt-demo',
